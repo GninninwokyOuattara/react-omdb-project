d55a6ca21ac499bfa607b7dce82c86afb212a597
Gninninwoky Ouattara <idrissgouattara@gmail.com>
2020-12-23 01:20:08 +0000

    A least now preventDefault ain't an issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import "./components/SearchBar";
 import SearchBar from "./components/SearchBar";
 
 function App() {
-    return <SearchBar />;
+    const [moviesData, setMoviesData] = useState<{}>({});
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    return <SearchBar props={{ setMoviesData, setIsLoading }} />;
 }
 
 export default App;
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,56 +4,46 @@ import "./SearchBar.css";
 
 type searchState = "null" | "string";
 
-const SearchBar: React.FC = (props) => {
+interface ParamState {
+    props: {
+        setMoviesData: Function;
+        setIsLoading: Function;
+    };
+}
+
+interface Response {
+    Search?: {}[];
+    Response: boolean;
+    totalResults: string;
+}
+
+interface ResponseData {
+    (): Promise<{}>;
+}
+
+const SearchBar: React.FC<ParamState> = (props) => {
+    // console.log({ props });
     const searchBarRef = useRef() as React.MutableRefObject<HTMLInputElement>;
 
-    const clickHandler = () => {
-        console.log(searchBarRef.current.value);
-        searchBarRef.current.value = "";
-    };
-
-    const fetchFromApi = async () => {
-        const res = await fetch(
-            "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/*",
-            {
-                method: "GET",
-                headers: {
-                    "x-rapidapi-key":
-                        "5dcc4627a8mshabdde2657854622p105f95jsn912ffefb58d9",
-                    "x-rapidapi-host":
-                        "imdb-internet-movie-database-unofficial.p.rapidapi.com",
-                },
-            }
-        );
+    const fetchData = async (): Promise<Response> => {
+        const query = `http://www.omdbapi.com/?s=${
+            searchBarRef.current.value
+        }=${new Date().getFullYear().toString()}&apikey=480344f1&r`;
+        console.log(query);
+        const res = await fetch(query);
         const response = await res.json();
-
-        console.log("from APi", response);
+        return response;
     };
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const query = `http://www.omdbapi.com/?y=${new Date()
-                .getFullYear()
-                .toString()}&apikey=480344f1&r`;
-            console.log(query);
-            const res = await fetch(query);
-            const response = await res.json();
-            console.log(response);
-        };
-
-        fetchData();
-        fetchFromApi();
-        return () => {
-            // cleanup
-        };
-    }, []);
-
-    const submitHandler = () => {
-        console.log(searchBarRef.current.value);
+    const submitHandler = (e: React.FormEvent) => {
+        console.log("hey");
+        e.preventDefault();
+        const moviesData = fetchData();
+        console.log(moviesData);
     };
     return (
         <div className="d-flex flex-column justify-content-center align-items-center">
-            <form action="" className="form">
+            <form action="" className="form" onSubmit={submitHandler}>
                 <div className="input-container my-4 rounded">
                     <i className="fal fa-search"></i>
                     <input
@@ -66,7 +56,6 @@ const SearchBar: React.FC = (props) => {
                 <button
                     type="submit"
                     className="form__btn-submit w-50 d-block mx-auto rounded border-0 text-white"
-                    onSubmit={submitHandler}
                 >
                     Search
                 </button>
